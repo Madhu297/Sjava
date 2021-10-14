21ef19e4e8125c952daa289147fdfba56dfc417e
Madhu297 <madhuarya.sg@gmail.com>
2021-10-14 21:52:51 +0800

    My First Commit...

---
+++ src/Java/basics/practice/Java_Practice.java
@@ -0,0 +1,33 @@
+package Java.basics.practice;
+
+public class Java_Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+	    boolean flag = true;
+	    System.out.println(flag); 
+	    
+        float f1 = 1.5f;
+        float f2 = 2.5f;
+        
+        float p = f1*f2;
+        
+        System.out.println("The product is: " +p);
+        
+        int first = 10;
+        int second = 20;
+        
+        System.out.println(first + " " +second);
+        
+        int sum = first + second;
+        
+        System.out.println("The sume is: "+sum);
+        
+       
+	    
+	}
+
+}
